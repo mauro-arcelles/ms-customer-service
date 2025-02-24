c061b7b3446aa31d8163f2c9c89788f728a40f0d
mauro-arcelles <mauroar1001@hotmail.com>
2025-02-24 12:56:34 -0500

    refactor: add logs to global exception handler

--- src/main/java/com/project1/ms_customer_service/exception/GlobalExceptionHandler.java
+++ src/main/java/com/project1/ms_customer_service/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.fasterxml.jackson.databind.exc.InvalidTypeIdException;
 import com.project1.ms_customer_service.model.CustomerRequest;
 import com.project1.ms_customer_service.model.ResponseBase;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.codec.DecodingException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,10 +21,12 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(WebExchangeBindException.class)
     public Mono<ResponseEntity<Map<String, List<String>>>> handleValidationErrors(WebExchangeBindException ex) {
+        log.error("Error", ex);
         Map<String, List<String>> errors = ex.getBindingResult()
                 .getFieldErrors()
                 .stream()
@@ -36,13 +39,14 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public Mono<ResponseEntity<String>> handleGenericError(Exception ex) {
-        ex.printStackTrace();
+        log.error("Error", ex);
         return Mono.just(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body("Internal Server Error"));
     }
 
     @ExceptionHandler(NotFoundException.class)
     public Mono<ResponseEntity<ResponseBase>> handleNotFoundException(Exception ex) {
+        log.error("Error", ex);
         ResponseBase responseBase = new ResponseBase();
         responseBase.setMessage(ex.getMessage());
         return Mono.just(ResponseEntity.status(HttpStatus.NOT_FOUND)
@@ -51,6 +55,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(InvalidCustomerTypeException.class)
     public Mono<ResponseEntity<ResponseBase>> handleInvalidCustomerTypeException(Exception ex) {
+        log.error("Error", ex);
         ResponseBase responseBase = new ResponseBase();
         responseBase.setMessage(ex.getMessage());
         return Mono.just(ResponseEntity.status(HttpStatus.BAD_REQUEST)
@@ -59,6 +64,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(BadRequestException.class)
     public Mono<ResponseEntity<ResponseBase>> handleBadRequestException(Exception ex) {
+        log.error("Error", ex);
         ResponseBase responseBase = new ResponseBase();
         responseBase.setMessage(ex.getMessage());
         return Mono.just(ResponseEntity.status(HttpStatus.BAD_REQUEST)
@@ -67,6 +73,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(ServerWebInputException.class)
     public Mono<ResponseEntity<Map<String, List<String>>>> handleServerWebInputException(ServerWebInputException ex) {
+        log.error("Error", ex);
         Map<String, List<String>> errors = new HashMap<>();
 
         if (ex.getCause() instanceof DecodingException) {
